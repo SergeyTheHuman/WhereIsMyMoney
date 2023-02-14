45fdf95cf72e6c060e1e2858094ad9f2729f3a54
Sergey <sergey2611.ivanov@yandex.ru>
2023-02-14 17:18:01 +0600

    Added mocks

---
+++ backend/src/mocks/users.js
@@ -0,0 +1,67 @@
+export const users = [
+	{
+	  "id": "63eb6dc2eebe62cad16f6bae",
+	  "email": "diaz_davis@undefined.lu",
+	  "apiKey": "2bf129a2-d0c7-44e5-95c9-e8944f787fc9",
+	  "profile": {
+		 "name": "Diaz Davis",
+		 "about": "Magna laborum laborum commodo quis enim Lorem laboris. Adipisicing velit amet aute dolore id eiusmod labore incididunt nisi ad tempor commodo mollit labore.",
+		 "address": "47 Louisa Street, Aberdeen, New Mexico"
+	  },
+	  "username": "diaz23",
+	  "createdAt": "2013-02-21T23:18:33.991Z",
+	  "updatedAt": "2013-02-22T23:18:33.991Z"
+	},
+	{
+	  "id": "63eb6dc268d9d02514c52772",
+	  "email": "francine_guy@undefined.organic",
+	  "apiKey": "4a29b12b-bffe-44c0-b358-12ae7bc52433",
+	  "profile": {
+		 "name": "Francine Guy",
+		 "about": "Aliquip in eiusmod non dolore sunt eiusmod ad incididunt cupidatat. Aliqua magna dolore tempor esse occaecat quis adipisicing deserunt culpa Lorem non deserunt.",
+		 "address": "50 Vanderveer Street, Fairhaven, Northern Mariana Islands"
+	  },
+	  "username": "francine23",
+	  "createdAt": "2011-09-12T23:34:33.710Z",
+	  "updatedAt": "2011-09-13T23:34:33.710Z"
+	},
+	{
+	  "id": "63eb6dc28509671509ac9ad1",
+	  "email": "rachel_carr@undefined.jewelry",
+	  "apiKey": "99d3813f-dc62-4b7d-8111-060d27b4e649",
+	  "profile": {
+		 "name": "Rachel Carr",
+		 "about": "Aute commodo minim excepteur excepteur esse deserunt cillum. Sit sunt anim consequat ea et nisi commodo culpa deserunt enim labore.",
+		 "address": "66 Bergen Court, Whitehaven, Vermont"
+	  },
+	  "username": "rachel23",
+	  "createdAt": "2013-02-14T19:34:47.956Z",
+	  "updatedAt": "2013-02-15T19:34:47.956Z"
+	},
+	{
+	  "id": "63eb6dc28f10136fd0ecccc0",
+	  "email": "tricia_todd@undefined.cc",
+	  "apiKey": "e13e57d8-1f12-43bd-8ee6-2ba5a7f9c321",
+	  "profile": {
+		 "name": "Tricia Todd",
+		 "about": "Sint elit non reprehenderit do ea sint qui enim. Voluptate labore duis ut consectetur sunt duis ullamco consectetur ex.",
+		 "address": "94 Vernon Avenue, Sperryville, Michigan"
+	  },
+	  "username": "tricia23",
+	  "createdAt": "2010-11-14T02:29:29.419Z",
+	  "updatedAt": "2010-11-15T02:29:29.419Z"
+	},
+	{
+	  "id": "63eb6dc2dcaa868f3ff1536f",
+	  "email": "rosales_clayton@undefined.tatar",
+	  "apiKey": "045ecf60-13b5-4e3d-a545-0e3ffe7bf3e6",
+	  "profile": {
+		 "name": "Rosales Clayton",
+		 "about": "Commodo ullamco incididunt ad aliquip in ipsum commodo deserunt. Tempor incididunt ullamco eu velit laborum cillum irure quis sit.",
+		 "address": "87 Drew Street, Coultervillle, Federated States Of Micronesia"
+	  },
+	  "username": "rosales23",
+	  "createdAt": "2012-03-19T00:15:20.896Z",
+	  "updatedAt": "2012-03-20T00:15:20.896Z"
+	}
+ ]
