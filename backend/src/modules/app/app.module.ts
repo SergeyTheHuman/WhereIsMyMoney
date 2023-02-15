fe2dd0c50dbe5fc1503228b985efaad3a389786e
Sergey <sergey2611.ivanov@yandex.ru>
2023-02-15 10:23:58 +0600

    Added connection to db + redis

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import configurations from 'src/configurations'
 import { UsersModule } from 'src/modules/users/users.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { SequelizeModule } from '@nestjs/sequelize'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 			load: [configurations],
+		}),
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				dialect: configService.get('db_dialect'),
+				host: configService.get('db_host'),
+				port: configService.get('db_port'),
+				username: configService.get('db_user'),
+				password: configService.get('db_password'),
+				database: configService.get('db_database'),
+				synchronize: true,
+				autoLoadModels: true,
+				models: [],
+			})
 		}),
 		UsersModule,
 	],
