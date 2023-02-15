908fe3a430650445e4eb7b20539f017f99f2255c
Sergey <sergey2611.ivanov@yandex.ru>
2023-02-15 11:13:48 +0600

    Added validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
@@ -7,15 +8,16 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const configService = app.get(ConfigService)
 	const port = configService.get('port')
+	app.useGlobalPipes(new ValidationPipe())
 
 	const configSwagger = new DocumentBuilder()
 		.setTitle('WhereIsMyMoney api')
 		.setDescription('This is an api for WIMM application')
 		.setVersion('1.0')
 		.build()
+		
 	const documentSwagger = SwaggerModule.createDocument(app, configSwagger)
 	SwaggerModule.setup('api', app, documentSwagger)
-	
 	await app.listen(port, () => console.log(`Server is listening on port ${port}`))
 }
 bootstrap()
